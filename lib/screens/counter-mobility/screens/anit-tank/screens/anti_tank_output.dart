6fd1637c72005976160670418a3c7a574f91b772
Md Rafee <md.rafee@selise.ch>
2020-10-02 02:45:27 +0600

    image download testing

--- lib/screens/counter-mobility/screens/anit-tank/screens/anti_tank_output.dart
+++ lib/screens/counter-mobility/screens/anit-tank/screens/anti_tank_output.dart
@@ -1,4 +1,10 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:ui' as ui;
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 import '../../../../../router/route_const.dart';
 import '../../../../../shared/widgets/top_header.dart';
@@ -8,9 +14,22 @@ import '../../reserve-demolition/widgets/time_requirement.dart';
 import '../models/anti_tank.dart';
 
 class AntiTankOutput extends StatelessWidget {
+  static GlobalKey screen = new GlobalKey();
+  screenShot() async {
+    RenderRepaintBoundary boundary = screen.currentContext.findRenderObject();
+    ui.Image image = await boundary.toImage();
+    final directory = (await getApplicationSupportDirectory()).path;
+    ByteData byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    Uint8List pngBytes = byteData.buffer.asUint8List();
+    print(pngBytes);
+    File imgFile = new File('$directory/screenshot.png');
+    imgFile.writeAsBytes(pngBytes);
+  }
+
   @override
   Widget build(BuildContext context) {
     final AntiTank _model = ModalRoute.of(context).settings.arguments;
+
     final AppBar appbar = new AppBar(
       title: Text('Anti Tank Ditch'),
       actions: <Widget>[
@@ -23,75 +42,82 @@ class AntiTankOutput extends StatelessWidget {
               ModalRoute.withName(counterMobilityScreen),
             );
           },
-        )
+        ),
+        IconButton(
+          icon: const Icon(Icons.file_download),
+          onPressed: screenShot,
+        ),
       ],
     );
     return Scaffold(
       appBar: appbar,
-      body: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.all(10),
-          child: Column(
-            children: [
-              TopHeader("Calculation of Anti-tank ditch"),
-              SummaryOfCalculation(),
-              Container(
-                padding: EdgeInsets.only(left: 20),
-                alignment: Alignment.topLeft,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "a. The width of anti-tank ditch = ${_model.width} yards",
-                    ),
-                    Text(
-                      "b. The height of anti-tank ditch = ${_model.height.toStringAsFixed(2)} yards",
-                    ),
-                    Text(
-                      "c. The volume of anti-tank ditch = ${_model.volume.toStringAsFixed(2)} yards",
-                    ),
-                  ],
+      body: RepaintBoundary(
+        key: screen,
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                TopHeader("Calculation of Anti-tank ditch"),
+                SummaryOfCalculation(),
+                Container(
+                  padding: EdgeInsets.only(left: 20),
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "a. The width of anti-tank ditch = ${_model.width} yards",
+                      ),
+                      Text(
+                        "b. The height of anti-tank ditch = ${_model.height.toStringAsFixed(2)} yards",
+                      ),
+                      Text(
+                        "c. The volume of anti-tank ditch = ${_model.volume.toStringAsFixed(2)} yards",
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              TimeRequirement(),
-              Container(
-                padding: EdgeInsets.only(left: 20),
-                alignment: Alignment.topLeft,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "a. Time requirement for 1 Dozon = ${_model.timeRequiredPerDozon.toStringAsFixed(2)} hours",
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "Note: Capacity of size ii/iv Dozer with 100' half way hauling is 55 cubic yard/hour. (Auth: GSTP-1608Figure 50 page 184)",
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "b. Total time require for plant platoon = ${_model.totalTimeRequired.toStringAsFixed(2)} Platoon hours",
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "Note: 1x Plant Platoon has 4x Dozer (2x Size-II 2x Size IV)",
-                    ),
-                  ],
+                TimeRequirement(),
+                Container(
+                  padding: EdgeInsets.only(left: 20),
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "a. Time requirement for 1 Dozon = ${_model.timeRequiredPerDozon.toStringAsFixed(2)} hours",
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Note: Capacity of size ii/iv Dozer with 100' half way hauling is 55 cubic yard/hour. (Auth: GSTP-1608Figure 50 page 184)",
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "b. Total time require for plant platoon = ${_model.totalTimeRequired.toStringAsFixed(2)} Platoon hours",
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Note: 1x Plant Platoon has 4x Dozer (2x Size-II 2x Size IV)",
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              SectionHeading('3. ', 'Layout of Anti-tank Ditch'),
-              Container(
-                child: Image.asset(
-                  'assets/images/anti-tank-ditch.png',
-                  fit: BoxFit.cover,
+                SectionHeading('3. ', 'Layout of Anti-tank Ditch'),
+                Container(
+                  child: Image.asset(
+                    'assets/images/anti-tank-ditch.png',
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
